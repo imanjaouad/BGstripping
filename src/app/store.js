c5fcbf2875372c096abfa9173e3a4c0907df6884
imane <imanjaouad480@gmail.com>
2026-03-09 10:42:23 +0000

    Clean: remove src, public and root config files

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import poussageReducer from "../features/poussageSlice";
-
-export const store = configureStore({
-  reducer: {
-    poussage: poussageReducer
-  }
-});
