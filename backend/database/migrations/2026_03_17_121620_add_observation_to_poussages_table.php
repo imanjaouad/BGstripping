c669345c2e461278ee22d89707843d22298f6940
keltoum <keltoumbennour830@gmail.com>
2026-03-30 11:30:08 +0100

    backend

--- backend/database/migrations/2026_03_17_121620_add_observation_to_poussages_table.php
+++ backend/database/migrations/2026_03_17_121620_add_observation_to_poussages_table.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -10,29 +8,21 @@ return new class extends Migration
 {
     public function up(): void
     {
+       Schema::table('poussages', function (Blueprint $table) {
+    if (!Schema::hasColumn('poussages', 'HTP')) {
+        $table->decimal('HTP', 8, 2)->nullable();
+    }
 
-      Schema::table('poussages', function (Blueprint $table) {
-    $table->text('observation')->nullable();
+    if (!Schema::hasColumn('poussages', 'observation')) {
+        $table->text('observation')->nullable();
+    }
 });
-
-        Schema::table('poussages', function (Blueprint $table) {
-
-            $table->number('HTP')->nullable();
-            $table->decimal('observation', 8, 2)->nullable();
-        });
-
-      
-        
-
-
     }
 
     public function down(): void
     {
         Schema::table('poussages', function (Blueprint $table) {
-
-            $table->dropColumn('HTP');
-            $table->dropColumn('observation');
+            $table->dropColumn(['HTP', 'observation']);
         });
     }
 };
